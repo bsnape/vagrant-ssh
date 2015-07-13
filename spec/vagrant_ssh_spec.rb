194b1b0cd36271a1debe3aed63fcfc57172e1760
Ben Snape <ben.snape@itv.com>
2015-07-13 17:00:11 +0100

    refactored tests to use vagrant_ssh subject

--- spec/vagrant_ssh_spec.rb
+++ spec/vagrant_ssh_spec.rb
@@ -1,14 +1,15 @@
 describe VagrantSsh::Shell do
+  subject(:vagrant_ssh) { VagrantSsh::Shell.new(host, nil_logger) }
+
   let(:credentials) { { :user => 'vagrant', :password => 'vagrant' } }
   let(:nil_logger) { Logger.new(nil) }
   let(:host) { '33.33.33.33' }
 
   it 'defaults to the standard Vagrant credentials' do
-    expect(VagrantSsh::Shell.new(host, nil_logger).options).to eq credentials
+    expect(vagrant_ssh.options).to eq credentials
   end
 
   it 'should interface with a Vagrant VM correctly' do
-    @ssh = VagrantSsh::Shell.new(host, nil_logger, credentials)
-    expect(@ssh.execute('whoami').strip).to eq 'vagrant'
+    expect(vagrant_ssh.execute('whoami').strip).to eq 'vagrant'
   end
 end
