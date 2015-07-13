6e4de2003744c77ec2c54878b7361b26b7985a25
Ben Snape <ben.snape@itv.com>
2015-07-13 16:18:03 +0100

    moved VM creation/destruction out of the tests and into the Rakefile

--- Rakefile
+++ Rakefile
@@ -1,6 +1,20 @@
 require 'bundler/gem_tasks'
 require 'rspec/core/rake_task'
 
-RSpec::Core::RakeTask.new
+RSpec::Core::RakeTask.new(:spec) do |task|
+  task.rspec_opts = '--format documentation --color --require spec_helper'
+end
+
+desc 'Create Vagrant VM (required for running the tests)'
+task :create_vm do
+  ENV['VAGRANT_CWD'] = File.expand_path('spec/support')
+  p `vagrant up`
+end
+
+desc "Destroy Vagrant VM (once you're done with testing)"
+task :destroy_vm do
+  ENV['VAGRANT_CWD'] = File.expand_path('spec/support')
+  p `vagrant destroy -f`
+end
 
 task :default => :spec
--- spec/vagrant_ssh_spec.rb
+++ spec/vagrant_ssh_spec.rb
@@ -1,17 +1,14 @@
 describe Vagrant::SSH::Shell do
+  let(:credentials) { { :user => 'vagrant', :password => 'vagrant' } }
+  let(:nil_logger) { Logger.new(nil) }
+  let(:host) { '33.33.33.33' }
 
-  before(:all) do
-    ENV['VAGRANT_CWD'] = File.expand_path('spec/support')
-    `vagrant up`
-  end
-
-  after(:all) do
-    `vagrant destroy -f`
+  it 'defaults to the standard Vagrant credentials' do
+    expect(Vagrant::SSH::Shell.new(host, nil_logger).options).to eq credentials
   end
 
   it 'should interface with a Vagrant VM correctly' do
-    @ssh = Vagrant::SSH::Shell.new('33.33.33.33', Logger.new(nil), {:user => 'vagrant', :password => 'vagrant'})
-    @ssh.execute('whoami').strip.should == 'vagrant'
+    @ssh = Vagrant::SSH::Shell.new(host, nil_logger, credentials)
+    expect(@ssh.execute('whoami').strip).to eq 'vagrant'
   end
-
 end
